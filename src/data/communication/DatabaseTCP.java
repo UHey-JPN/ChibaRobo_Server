bc316abc807cb12ac2797d58c5db59e8ca5586d7
Yuhei mac <yumasingingAgmail.com>
2017-08-29 23:33:21 +0900

    [add] 画像ダウンロード機能を追加

--- src/data/communication/DatabaseTCP.java
+++ src/data/communication/DatabaseTCP.java
@@ -5,14 +5,16 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.Executor;
 
+import data.image.ImageList;
 import data.main.Database;
 
 public class DatabaseTCP implements Runnable {
 	private Executor ex;
 	private Database database;
+	private ImageList img_list;
 	private ServerSocket listen;
 	
-	public DatabaseTCP(Executor ex, Database database) {
+	public DatabaseTCP(Executor ex, Database database, ImageList img_list) {
 		this.ex = ex;
 		this.database = database;
 		
@@ -37,7 +39,7 @@ public class DatabaseTCP implements Runnable {
 		while(true){
 			try {
 				Socket soc = listen.accept();
-				ex.execute( new ThreadDatabase(soc, database) );
+				ex.execute( new ThreadDatabase(soc, database,img_list) );
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
