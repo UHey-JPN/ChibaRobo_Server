fe6e57e0248e4b1794be4513f15822a372ec4d6b
yusaku <yskhara97@gmail.com>
2018-09-27 02:46:24 +0900

    fix #1

--- src/main/ChibaRoboServer.java
+++ src/main/ChibaRoboServer.java
@@ -1,7 +1,11 @@
 package main;
 
+import java.net.Inet4Address;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Enumeration;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -21,17 +25,46 @@ public class ChibaRoboServer {
 
 	public ChibaRoboServer() {
 		Executor ex = Executors.newCachedThreadPool();
+
+		System.out.println("Starting ChibaRobo Server...");
+
+		InetAddress addr = null;
 		try {
-			InetAddress addr = InetAddress.getLocalHost();
-			System.out.println(addr.getHostName() + "/" + addr.getHostAddress());
+			addr = InetAddress.getLocalHost();
 		} catch (UnknownHostException e) {
+			System.out.println("An UnknownHostException was thrown while trying to get local IP addresses.");
 			e.printStackTrace();
 		}
-		
-		UdpSocket udp = new UdpSocket();
+
+		// enumerate all NIC
+		if (addr == null || addr.isLoopbackAddress()) {
+			try {
+				Enumeration<NetworkInterface> n = NetworkInterface.getNetworkInterfaces();
+				while ((addr == null || addr.isLoopbackAddress()) && n.hasMoreElements()) {
+					NetworkInterface e = n.nextElement();
+					System.out.println("Network interface detected: " + e.getName());
+					Enumeration<InetAddress> a = e.getInetAddresses();
+					while (a.hasMoreElements()) {
+						InetAddress _addr = a.nextElement();
+						System.out.println("  " + _addr.getHostAddress());
+						if(!_addr.isLoopbackAddress() && (_addr instanceof Inet4Address)) {
+							addr = _addr;
+							break;
+						}
+					}
+				}
+			} catch (SocketException e) {
+				System.out.println("An error occurred while trying to get local IP addresses.");
+				e.printStackTrace();
+			}
+		}
+
+		System.out.println(addr.getHostName() + "/" + addr.getHostAddress());
+
+		UdpSocket udp = new UdpSocket(addr);
 
 		LogToSystemIO log = new LogToSystemIO();
-		
+
 		ImageList img_list = new ImageList("DB/img/", log);
 		Database database = new Database(24);
 		KeepAliveManager kam = new KeepAliveManager(ex);
@@ -39,23 +72,23 @@ public class ChibaRoboServer {
 		DatabaseUdp database_udp = new DatabaseUdp(udp, database, ssm);
 		DatabaseTCP database_tcp = new DatabaseTCP(ex, database, img_list);
 		ConsoleSocket console = new ConsoleSocket(ex, database, ssm, kam, img_list);
-		Publicity publicity = new Publicity(ex, udp, console.get_local_port(), database_tcp.get_local_port(), kam.get_local_port());
-		
-		try{
+		Publicity publicity = new Publicity(ex, udp, console.get_local_port(), database_tcp.get_local_port(),
+				kam.get_local_port());
+
+		try {
 			new WindowMain(ex, udp, database, kam, ssm, database_udp, database_tcp, console, publicity);
-		} catch (java.awt.HeadlessException e){
+		} catch (java.awt.HeadlessException e) {
 			System.out.println("no window.main mode");
 		}
-		
+
 		System.out.println("Server System is OK");
 
-		
 	}
 
 	public static void main(String[] args) {
 		System.setProperty("file.encoding", "UTF-8");
 		new ChibaRoboServer();
-		
+
 	}
 
 }
--- src/udpSocket/UdpSocket.java
+++ src/udpSocket/UdpSocket.java
@@ -3,13 +3,17 @@ package udpSocket;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.Inet4Address;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.SocketException;
+import java.net.UnknownHostException;
 
 public class UdpSocket {
 	private final static String CRLF = "\r\n";
 	private final static int SOC_PORT = 58239;
 	private DatagramSocket soc;
+	private InetAddress addr;
 
 	public UdpSocket() {
 		try {
@@ -18,28 +22,50 @@ public class UdpSocket {
 		} catch (SocketException e) {
 			e.printStackTrace();
 		}
+
+		try {
+			this.addr = InetAddress.getLocalHost();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
 	}
-	
-	public int get_port(){
+
+	public UdpSocket(InetAddress addr) {
+		try {
+			soc = new DatagramSocket();
+			System.out.println("UDP Socket is opened(port number = " + soc.getLocalPort() + ").");
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+
+		this.addr = addr;
+	}
+
+	public InetAddress get_inet_address() {
+		// return soc.getLocalAddress();
+		return this.addr;
+	}
+
+	public int get_port() {
 		return soc.getLocalPort();
 	}
 
-	public synchronized void send_show(String packet){
+	public synchronized void send_show(String packet) {
 		send("show" + CRLF + packet);
 	}
 
-	public synchronized void send_tournament(String packet){
+	public synchronized void send_tournament(String packet) {
 		send("tournament" + CRLF + packet);
 	}
 
-	public synchronized void send_server(String packet){
+	public synchronized void send_server(String packet) {
 		send("server" + CRLF + packet);
 	}
 
-	private synchronized void send(String str_packet){
+	private synchronized void send(String str_packet) {
 		// set broadcast address
 		InetSocketAddress broadcast = new InetSocketAddress("255.255.255.255", SOC_PORT);
-		
+
 		try {
 			// create packet
 			byte[] b_packet = str_packet.getBytes("UTF-8");
@@ -49,7 +75,7 @@ public class UdpSocket {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
